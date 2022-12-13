5a7b34c8a82834f2bfae647d4a0d444bf038ff0d
brianthungu <brianthungu.bt@gmail.com>
2022-12-13 21:55:31 +0300

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	long int fibonacci[50], sum = 2;
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k
+		k = next;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
