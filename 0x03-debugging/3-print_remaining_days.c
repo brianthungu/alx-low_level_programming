2ee73691d83b05c9d21fae33c3cf3a660c1e597c
brianthungu <brianthungu.bt@gmail.com>
2022-12-17 01:18:01 +0300

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
 	{
-		if (month >= 2)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
@@ -27,7 +27,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 29, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
