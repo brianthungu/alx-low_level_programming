f42816137800bc67f5179bc71aa1fe0cd538f4c8
brianthungu <brianthungu.bt@gmail.com>
2022-12-28 12:48:52 +0300

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,52 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * base10 - power in 10 base
- * @n: an exponent
- * Return: returns 10 to power exponent
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
  */
-int base10(int n)
+
+char *rot13(char *s)
 {
-	int base = 10;
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (n > 0)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		base *= 10;
-		n--;
-	}
-	return (base);
-}
-
-/**
- * print_number - prints integers enters as parameters using putchar
- * @n: integer to print
- * Return: void
- */
-void print_number(int n)
-{
-	int power;
-
-	power = base10(8);
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
-
-	if (n == 0)
-		_putchar('0');
-
-	else
-	{
-		while (n / power == 0)
-			power /= 10;
-
-		while (power >= 1)
+		for (j = 0; j < 52; j++)
 		{
-			_putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
 		}
 	}
+	return (s);
 }
+
